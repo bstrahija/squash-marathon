0aee934f521bae334509958aabfd9887569d5dde
Boris Strahija <bstrahija@gmail.com>
2026-04-15 08:33:01 +0200

    Setup favicon

--- resources/views/tv-group.blade.php
+++ resources/views/tv-group.blade.php
@@ -5,6 +5,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>TV prikaz - Grupa {{ $groupNumber }}</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}" />
+    <link rel="manifest" href="{{ asset('images/site.webmanifest') }}" />
     @vite(['resources/css/app.css', 'resources/css/tv.css', 'resources/js/blade-theme.ts'])
     @livewireStyles
 </head>
